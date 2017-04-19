726294c317992d26ec34ab47a0a3b75ca980b83c
Sebastien Besson <seb.besson@gmail.com>
2017-04-19 22:51:15 +0100

    Add Travis script to populate ImageJ update site
    A per the instructions of
    http://imagej.net/Automatic_Update_Site_Uploads#Automatic_Uploads_via_Travis_CI
    this commit adds a simple Travis deployment script to install a new copy of
    Fiji, run `mvn clean package` and upload the new JARs to an update site.

---
+++ travis-deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+set -e
+
+export WEBDAV_USER="Sbesson"
+export UPDATE_SITE="Sbesson"
+
+# Define some variables 
+export IJ_PATH="$HOME/Fiji.app"
+export URL="http://sites.imagej.net/$UPDATE_SITE/"
+export IJ_LAUNCHER="$IJ_PATH/ImageJ-linux64"
+export PATH="$IJ_PATH:$PATH"
+
+# Install ImageJ
+mkdir -p $IJ_PATH/
+cd $HOME/
+wget --no-check-certificate https://downloads.imagej.net/fiji/latest/fiji-linux64.zip
+unzip fiji-linux64.zip
+$IJ_LAUNCHER --update update-force-pristine
+
+# Install the package
+cd $TRAVIS_BUILD_DIR/
+mvn clean package -Dfiji.home=$IJ_PATH
+
+# Deploy the package
+$IJ_LAUNCHER --update edit-update-site $UPDATE_SITE $URL "webdav:$WEBDAV_USER:$WIKI_UPLOAD_PASS" .
+$IJ_LAUNCHER --update upload-complete-site --force-shadow $UPDATE_SITE
